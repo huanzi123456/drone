cdc3c142ffb5892bacaa8b54f399a14c0fe84c70
huanzi123456 <45910510+huanzi123456@users.noreply.github.com>
2018-12-16 18:58:31 +0800

    Update Main.java

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -5,5 +5,8 @@ public class Main {
     @Test
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
+         System.out.println("HelloWorld!"); 
+        System.out.println("HelloWorld!");
+         System.out.println("HelloWorld!");
     }
 }
